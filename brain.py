1ce83a7757a903a2f3150cc4d719cf0c9be3ecbf
Idriss Foka <idrissfoka748@gmail.com>
2026-09-09 00:08:46 +0200

    Add rank and file (chess naming) and also return all the created chess piece objects into variables which can later on be made to move or interract as we progress.

--- brain.py
+++ brain.py
@@ -6,71 +6,90 @@ from turtle import color
 # Global variables
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
+font = pygame.font.SysFont("Noto Sans Symbols 2", 50 )
 clock = pygame.time.Clock()
 board_width = 600
 center_x = screen.get_width() / 2
 center_y = screen.get_height() / 2
-padding = 5
+player_1_color = (0, 0, 0)
+player_2_color = (255, 255, 255)
+player_1 = 1
+player_2 = 2
 increment = 75
 initial_x = 340
 initial_y = 60
 running = True
 clicked_square = {}
-font = pygame.font.SysFont("Noto Sans Symbols 2", 50 )
 def initial_player_pieces(color,player):
+    all_player_pieces = []
     if player == 1:
-        x = initial_x 
+        piece_file = initial_x 
         pawn_rank  = initial_y + increment
         piece_rank = initial_y
 
-        rook_1 = pieces.chess_piece("White_Rook", "♜", (color), player, font)
-        knight_1 = pieces.chess_piece("White_Knight", "♞", (color), player, font)
-        bishop_1 = pieces.chess_piece("White_Bishop", "♝", (color), player, font)
-        queen = pieces.chess_piece("White_Queen", "♛", (color), player, font)
-        king = pieces.chess_piece("White_King", "♚", (color), player, font)
-        bishop_2 = pieces.chess_piece("White_Bishop", "♝", (color), player, font)
-        knight_2 = pieces.chess_piece("White_Knight", "♞", (color), player, font)
-        rook_2 = pieces.chess_piece("White_Rook", "♜", (color), player, font)
-        screen.blit(rook_1.render_piece(font), (x + 10, piece_rank))
-        screen.blit(knight_1.render_piece(font), (x + 85, piece_rank))
-        screen.blit(bishop_1.render_piece(font), (x + 160, piece_rank))
-        screen.blit(queen.render_piece(font), (x + 235, piece_rank))
-        screen.blit(king.render_piece(font), (x + 310, piece_rank))
-        screen.blit(bishop_2.render_piece(font), (x + 385, piece_rank))
-        screen.blit(knight_2.render_piece(font), (x + 460, piece_rank))
-        screen.blit(rook_2.render_piece(font), (x + 535, piece_rank))
+        rook_1 = pieces.chess_piece("White_Rook", "♜", (color), player, font,screen, x=piece_file + 10, y=initial_y)
+        all_player_pieces.append(rook_1)
+        knight_1 = pieces.chess_piece("White_Knight", "♞", (color), player, font,screen, x=piece_file + 85, y=initial_y)
+        all_player_pieces.append(knight_1)
+        bishop_1 = pieces.chess_piece("White_Bishop", "♝", (color), player, font,screen, x=piece_file + 160, y=initial_y)
+        all_player_pieces.append(bishop_1)
+        queen = pieces.chess_piece("White_Queen", "♛", (color), player, font,screen, x=piece_file + 235, y=initial_y)
+        all_player_pieces.append(queen)
+        king = pieces.chess_piece("White_King", "♚", (color), player, font,screen, x=piece_file + 310, y=initial_y)
+        all_player_pieces.append(king)
+        bishop_2 = pieces.chess_piece("White_Bishop", "♝", (color), player, font,screen, x=piece_file + 385, y=initial_y)
+        all_player_pieces.append(bishop_2)
+        knight_2 = pieces.chess_piece("White_Knight", "♞", (color), player, font,screen, x=piece_file + 460, y=initial_y)
+        all_player_pieces.append(knight_2)
+        rook_2 = pieces.chess_piece("White_Rook", "♜", (color), player, font,screen, x=piece_file + 535, y=initial_y)
+        all_player_pieces.append(rook_2)
+        rook_1.render_piece(font)
+        knight_1.render_piece(font)
+        bishop_1.render_piece(font)
+        queen.render_piece(font)
+        king.render_piece(font)
+        bishop_2.render_piece(font)
+        knight_2.render_piece(font)
+        rook_2.render_piece(font)
+        for x in range(initial_x, initial_x + board_width, increment):
+            pawn = pieces.chess_piece("Black_Pawn", "♟", (color), player, font,screen, x=x + 10, y=pawn_rank)
+            pawn.render_piece(font)
+            all_player_pieces.append(pawn)
     elif player == 2:
-        x = initial_x
+        piece_file = initial_x
         pawn_rank  = board_width - increment
         piece_rank = board_width
 
-        rook_1 = pieces.chess_piece("Black_Rook", "♜", (color), player, font)
-        knight_1 = pieces.chess_piece("Black_Knight", "♞", (color), player, font)
-        bishop_1 = pieces.chess_piece("Black_Bishop", "♝", (color), player, font)
-        queen = pieces.chess_piece("Black_Queen", "♛", (color), player, font)
-        king = pieces.chess_piece("Black_King", "♚", (color), player, font)
-        bishop_2 = pieces.chess_piece("Black_Bishop", "♝", (color), player, font)
-        knight_2 = pieces.chess_piece("Black_Knight", "♞", (color), player, font)
-        rook_2 = pieces.chess_piece("Black_Rook", "♜", (color), player, font)
-        screen.blit(rook_1.render_piece(font), (x + 10, piece_rank))
-        screen.blit(knight_1.render_piece(font), (x + 85, piece_rank))
-        screen.blit(bishop_1.render_piece(font), (x + 160, piece_rank))
-        screen.blit(queen.render_piece(font), (x + 235, piece_rank))
-        screen.blit(king.render_piece(font), (x + 310, piece_rank))
-        screen.blit(bishop_2.render_piece(font), (x + 385, piece_rank))
-        screen.blit(knight_2.render_piece(font), (x + 460, piece_rank))
-        screen.blit(rook_2.render_piece(font), (x + 535, piece_rank))
+        rook_1 = pieces.chess_piece("Black_Rook", "♜", (color), player, font,screen, x=piece_file + 10, y=piece_rank)
+        all_player_pieces.append(rook_1)
+        knight_1 = pieces.chess_piece("Black_Knight", "♞", (color), player, font,screen, x=piece_file + 85, y=piece_rank)
+        all_player_pieces.append(knight_1)
+        bishop_1 = pieces.chess_piece("Black_Bishop", "♝", (color), player, font,screen, x=piece_file + 160, y=piece_rank)
+        all_player_pieces.append(bishop_1)
+        queen = pieces.chess_piece("Black_Queen", "♛", (color), player, font,screen, x=piece_file + 235, y=piece_rank)
+        all_player_pieces.append(queen)
+        king = pieces.chess_piece("Black_King", "♚", (color), player, font,screen, x=piece_file + 310, y=piece_rank)
+        all_player_pieces.append(king)
+        bishop_2 = pieces.chess_piece("Black_Bishop", "♝", (color), player, font,screen, x=piece_file + 385, y=piece_rank)
+        all_player_pieces.append(bishop_2)
+        knight_2 = pieces.chess_piece("Black_Knight", "♞", (color), player, font,screen, x=piece_file + 460, y=piece_rank)
+        all_player_pieces.append(knight_2)
+        rook_2 = pieces.chess_piece("Black_Rook", "♜", (color), player, font,screen, x=piece_file + 535, y=piece_rank)
+        all_player_pieces.append(rook_2)
+        rook_1.render_piece(font)
+        knight_1.render_piece(font)
+        bishop_1.render_piece(font)
+        queen.render_piece(font)
+        king.render_piece(font)
+        bishop_2.render_piece(font)
+        knight_2.render_piece(font)
+        rook_2.render_piece(font)
+        for x in range(initial_x, initial_x + board_width, increment):
+                pawn = pieces.chess_piece("Black_Pawn", "♟", (color), player, font,screen, x=x + 10, y=pawn_rank)
+                pawn.render_piece(font)
 
-    
-    for x in range(initial_x, initial_x + board_width, increment):
-        pawn = pieces.chess_piece("Black_Pawn", "♟", (color), player, font)
-        screen.blit(pawn.render_piece(font), (x + 10, pawn_rank))
+    return all_player_pieces
 
-    
-    
-    return None
-
-  
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -80,8 +99,6 @@ while running:
         elif event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:  # Left click
                 clicked_square = {"x": event.pos[0], "y": event.pos[1]}
-                #print(clicked_square["x"])
-                #print(pygame.font.get_fonts())
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill((102,51, 0))
@@ -91,20 +108,24 @@ while running:
 
     for x in range(initial_x, initial_x + board_width, increment):
         for y in range(initial_y, initial_y + board_width, increment):
-            if ((x-initial_x) +(y-initial_y)) % (increment * 2) == 0: #check if the sum of the x and y offsets is even to determine the color of the square
+
+            if ((x-initial_x) + (y-initial_y)) % (increment * 2) == 0:
                 color = (51, 104, 75)
             else:
                 color = (190, 202, 193)
-            pygame.draw.rect(screen, color, pygame.Rect(x, y, increment, increment))
-            initial_player_pieces((0, 0, 0), 1)
-            initial_player_pieces((255, 255, 255), 2)
 
-            if not clicked_square:
-                a = 0
-            elif (x <= clicked_square["x"] <= x+increment and y <= clicked_square["y"] <= y+increment ): # check if current square is clicked ... 
-                pygame.draw.rect(screen, "green", pygame.Rect(x, y, increment, increment),3)
-                
+            pygame.draw.rect(
+                screen,
+                color,
+                pygame.Rect(x, y, increment, increment)
+            )
+
+    # Pieces
+    player_1_pieces = initial_player_pieces(player_1_color, player_1)
+    player_2_pieces = initial_player_pieces(player_2_color, player_2)
+
+    print(player_1_pieces[0].name)
+    print(player_2_pieces)
     # flip() the display to put your work on screen
     pygame.display.flip()
-
-    clock.tick(60)  # limits FPS to 60
+    clock.tick(1)  # limits FPS to 60
--- pieces.py
+++ pieces.py
@@ -1,9 +1,13 @@
 class chess_piece:
-    def __init__(self, name, u_value, color, player,font):
+    def __init__(self, name, u_value, color, player,font,screen, x=0, y=0):
         self.name = name
         self.u_value = u_value
         self.color = color
         self.player = player
         self.font = font
+        self.screen = screen
+        self.x = x
+        self.y = y
     def render_piece(self, font):
-        return font.render(self.u_value, True, self.color)
+        piece = font.render(self.u_value, True, self.color)
+        self.screen.blit(piece, (self.x, self.y))
